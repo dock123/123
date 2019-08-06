605d2b9ce3e7771835eea8cf408acd749490e3c8
Evgeniy <ekuriga@inbox.ru>
2019-08-06 11:20:42 +0300

    bot

--- MyBot.py
+++ MyBot.py
@@ -1,6 +1,7 @@
 import variable as vb
 import requests
 import re
+import Weather as wt
 
 #bot
 upd = vb.methods['updates']
@@ -16,39 +17,69 @@ def get_update():
     return res.json()
     pass
 
-
+#Chat ID
 def get_chat_id():
     return get_update()['result'][-1]['message']['chat']['id']
     pass
 
-
+#Last message
 def get_last_message():
     return get_update()['result'][-1]['message']['text']
     pass
 
 
-def parse_text():
+def parse_text_money():
     pattern=r'/\w+'
     return re.search(pattern, get_last_message()).group()
 
-
-def get_update_money():
-    res = requests.get(url_money.format(parse_text()))
+#Update message
+def get_updates(message):
+    res = requests.get(url_money.format(message))
     return res.json()
     pass
 
-
-def get_money():
-    date=get_update_money()['Date']
-    name=get_update_money()['Cur_Name']
-    rate=get_update_money()['Cur_OfficialRate']
+#Get rates
+def get_money(message):
+    date=get_updates(message)['Date']
+    name=get_updates(message)['Cur_Name']
+    rate=get_updates(message)['Cur_OfficialRate']
     return 'За {}, курс {}, за 1 {}'.format(date, rate, name)
     pass
 
+#api_key='c200df268902d5a73de7e6b6563f745e'
+api_weather='http://api.openweathermap.org/data/2.5/find?q{}&type=like&lang=ru&APPID=c200df268902d5a73de7e6b6563f745e'
 
+#Update weather
+def get_weather_update(city):
+    res=requests.get(api_weather.format(city))
+    return res.json()
+    pass
+
+#Get weather
+def get_weather_message(m):
+    name=get_weather_update(m)['list'][0]['name']
+    conditions=get_weather_update(m)['list'][0]['weather'][0]['description']
+    temp=get_weather_update(m)['list'][0]['main']['temp']
+    temp_min=get_weather_update(m)['list'][0]['main']['temp_min']
+    temp_max=get_weather_update(m)['list'][0]['main']['temp_max']
+    return 'Город: {}, температура: {} , {}'.format(name, temp ,conditions)
+
+
+#Help
+def get_message():
+    if '/' in get_last_message():
+        return get_money(parse_text_money())
+    elif '=' in get_last_message():
+        return get_weather_message(get_last_message())
+    else:
+        return 'Для вывода валюты введите /usd\n' \
+               'Для вывода погоды введите =Brest,BY'
+
+
+#Send Message
 def send_message():
     chat_id=get_chat_id()
-    text=get_money()
+    text=get_message()
     params = {'chat_id': chat_id, 'text': text}
     requests.post(tel_api_url.format(token) + send, params)
 
